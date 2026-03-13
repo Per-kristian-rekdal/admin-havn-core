7f77bcdf84476a26f4039cabdebb4d5a7173af93
Per-kristian-rekdal <151571171+Per-kristian-rekdal@users.noreply.github.com>
2026-03-13 15:30:31 +0100

    Refactor havnekart-admin.php layout and structure

--- modules/havnekart/havnekart-admin.php
+++ modules/havnekart/havnekart-admin.php
@@ -1,41 +1,50 @@
 <?php
+if (!defined('ABSPATH')) exit;
 
 $map_id = get_option('ah_havnekart_image');
 $map_url = $map_id ? wp_get_attachment_url($map_id) : '';
-?>
-
-<h2>Kartbilde</h2>
-
-<input type="hidden" id="ah_havnekart_image" name="ah_havnekart_image" value="<?php echo esc_attr($map_id); ?>">
-
-<button class="button" id="ah_select_map">Velg kart</button>
-
-<br><br>
-
-<?php if($map_url): ?>
-<img src="<?php echo esc_url($map_url); ?>" style="max-width:100%;">
-<?php endif; ?>
-if (!defined('ABSPATH')) exit;
 ?>
 
 <div class="wrap">
 
 <h1>Havnekart – Kalibrering av pirer</h1>
 
-<p>Klikk start og slutt på pirene.</p>
+<h2>Kartbilde</h2>
+
+<input type="hidden" id="ah_havnekart_image" value="<?php echo esc_attr($map_id); ?>">
+
+<button class="button" id="ah_select_map">Velg kart</button>
+
+<br><br>
+
+<?php if($map_url): ?>
 
 <div style="position:relative;max-width:1200px">
 
-<img 
-src="<?php echo AH_PLUGIN_URL.'assets/havnekart-base.png'; ?>" 
+<img
+src="<?php echo esc_url($map_url); ?>"
 style="width:100%;display:block"
 id="havnekart-base">
 
 </div>
 
+<?php else: ?>
+
+<p><b>Ingen kart valgt.</b></p>
+
+<?php endif; ?>
+
 <hr>
 
-<h2>Pir 1</h2>
+<h2>Pirer</h2>
+
+<?php
+$piers = ['1','2','3'];
+
+foreach($piers as $pir){
+?>
+
+<h3>Pir <?php echo $pir; ?></h3>
 
 <button class="button">Velg start</button>
 <button class="button">Velg slutt</button>
@@ -47,23 +56,13 @@ id="havnekart-base">
 <input type="text">
 
 <label>Pir lengde (meter)</label>
-<input type="text>
+<input type="text">
 
 <hr>
 
-<h2>Pir 2</h2>
-
-<button class="button">Velg start</button>
-<button class="button">Velg slutt</button>
-
-<hr>
-
-<h2>Pir 3</h2>
-
-<button class="button">Velg start</button>
-<button class="button">Velg slutt</button>
-
-<br><br>
+<?php
+}
+?>
 
 <button class="button button-primary">Lagre pirer</button>
 
