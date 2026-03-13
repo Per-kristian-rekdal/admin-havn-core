23ecb1fc7984a9be3e3292f5df650db8301dfef3
Per-kristian-rekdal <151571171+Per-kristian-rekdal@users.noreply.github.com>
2026-03-13 11:44:43 +0100

    Refactor havnekart shortcode to use dynamic image
    Updated shortcode to use an image from options and added a fallback message if no image is selected.

--- modules/havnekart/havnekart.php
+++ modules/havnekart/havnekart.php
@@ -1,24 +1,24 @@
 <?php
 
-require_once __DIR__.'/renderer.php';
-
+// Shortcode for å vise havnekart
 function ah_havnekart_shortcode(){
 
-    ob_start();
+    $image_id = get_option('admin_havn_map_image_id');
 
+    if(!$image_id){
+        return "<p>Ingen havnekart valgt.</p>";
+    }
+
+    $map_url = wp_get_attachment_url($image_id);
+
+    ob_start();
     ?>
 
-    <div id="havnekart-wrapper" style="position:relative">
+    <div style="position:relative">
 
-        <img 
-        src="<?php echo plugin_dir_url(__FILE__).'/../../assets/havnekart-base.png'; ?>"
-        style="width:100%;display:block">
+        <img src="<?php echo esc_url($map_url); ?>" style="width:100%;display:block">
 
-        <svg id="havnekart-layer"
-        style="position:absolute;top:0;left:0;width:100%;height:100%">
-
-        <?php ah_render_map(); ?>
-
+        <svg style="position:absolute;top:0;left:0;width:100%;height:100%">
         </svg>
 
     </div>
