b7d0348a11443136229d3428570c10d36f697d5a
Per-kristian-rekdal <151571171+Per-kristian-rekdal@users.noreply.github.com>
2026-03-13 11:28:50 +0100

    Add geometry functions for distance and direction

--- modules/havnekart/geometry.php
+++ modules/havnekart/geometry.php
@@ -1,1 +1,25 @@
 
+<?php
+
+function ah_distance($x1,$y1,$x2,$y2){
+
+    return sqrt(($x2-$x1)*($x2-$x1)+($y2-$y1)*($y2-$y1));
+}
+
+function ah_direction($x1,$y1,$x2,$y2){
+
+    $len = ah_distance($x1,$y1,$x2,$y2);
+
+    return [
+        "dx"=>($x2-$x1)/$len,
+        "dy"=>($y2-$y1)/$len
+    ];
+}
+
+function ah_normal($dx,$dy){
+
+    return [
+        "nx"=>-$dy,
+        "ny"=>$dx
+    ];
+}
